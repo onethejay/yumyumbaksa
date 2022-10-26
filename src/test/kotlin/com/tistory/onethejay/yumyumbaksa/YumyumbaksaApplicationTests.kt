7a1e8cd1095215f5aaebec69df107184eba9f24d
onethejay <jhjang951@gmail.com>
2022-10-27 01:08:36 +0900

    Initial commit

---
+++ src/test/kotlin/com/tistory/onethejay/yumyumbaksa/YumyumbaksaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.onethejay.yumyumbaksa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YumyumbaksaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
