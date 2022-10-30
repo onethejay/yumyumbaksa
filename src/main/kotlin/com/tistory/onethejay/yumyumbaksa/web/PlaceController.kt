49f61f2f91a24571404d32ff283f026604e7a11b
onethejay <jhjang951@gmail.com>
2022-10-31 00:57:18 +0900

    맛집관리 컨트롤러 생성

---
+++ src/main/kotlin/com/tistory/onethejay/yumyumbaksa/web/PlaceController.kt
@@ -0,0 +1,22 @@
+﻿package com.tistory.onethejay.yumyumbaksa.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/place")
+class PlaceController {
+
+    @GetMapping("")
+    fun getPlace(): HashMap<String, String> {
+        val rtnMap: HashMap<String, String> = HashMap()
+        rtnMap.put("idx", "1")
+        rtnMap.put("placeName", "성수동 카레맛집")
+
+        return rtnMap
+    }
+
+}
+
+
