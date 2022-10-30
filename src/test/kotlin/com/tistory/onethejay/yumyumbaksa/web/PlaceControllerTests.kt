72031058cbd359ac0cb8174da802c307ca08ad81
onethejay <jhjang951@gmail.com>
2022-10-31 01:07:51 +0900

    테스트 콘솔 한글 깨짐 수정

--- src/test/kotlin/com/tistory/onethejay/yumyumbaksa/web/PlaceControllerTests.kt
+++ src/test/kotlin/com/tistory/onethejay/yumyumbaksa/web/PlaceControllerTests.kt
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.ResultActions
-import org.springframework.test.web.servlet.ResultHandler
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+//import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+
 
 @WebMvcTest
 internal class PlaceControllerTests {
@@ -20,7 +23,7 @@ internal class PlaceControllerTests {
     @Autowired
     lateinit var placeController: PlaceController
 
-    lateinit var mockMvc: MockMvc
+    private lateinit var mockMvc: MockMvc
 
     @BeforeEach
     fun setUp() {
@@ -32,12 +35,15 @@ internal class PlaceControllerTests {
     @Test
     fun test_1(){
         //when
-        val result: ResultActions = mockMvc.perform(get("/api/v1/place"))
+        val result: ResultActions = mockMvc.perform(get("/api/v1/place")
+            .contentType(MediaType.APPLICATION_JSON))
+
 
         //then
-        val mvcResult: MvcResult = result.andExpect(status().isOk)
+        val mvcResult: MvcResult = result.andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk)
             .andReturn()
 
-        assertThat(mvcResult.response.contentAsString).contains("성수동 카레맛집")
+        assertThat(mvcResult.response.getContentAsString(Charsets.UTF_8)).contains("성수동 카레맛집")
     }
 }
